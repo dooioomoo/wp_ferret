357d5da06a1f513251632480fd748b911b75ab46
dooioomoo <dooioomoo@gmail.com>
2019-12-03 15:41:10 +0900

    Update add_action.php

--- inc/base/add_action.php
+++ inc/base/add_action.php
@@ -61,3 +61,16 @@ function _ferret_custom_upload_name( $file ) {
 	return $file;
 }
 endif;
+
+add_filter( 'get_the_archive_title', function ($title) {    
+    if ( is_category() ) {    
+            $title = single_cat_title( '', false );    
+        } elseif ( is_tag() ) {    
+            $title = single_tag_title( '', false );    
+        } elseif ( is_author() ) {    
+            $title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+        } elseif ( is_tax() ) { //for custom post types
+            $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+        }    
+    return $title;    
+});
