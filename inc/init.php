2f5c6ea4c6bafc986e94679fcca2c60da1ecf973
dooioomoo <dooioomoo@gmail.com>
2020-09-13 06:07:06 +0900

    add clear head

--- inc/init.php
+++ inc/init.php
@@ -91,3 +91,39 @@ if ( !function_exists('_ferret_custom_header_setup') ):
         )));
     }
 endif;
+
+
+add_action('after_setup_theme', '_ferret_cleanup');
+
+if (!function_exists('_ferret_cleanup')) {
+
+	function _ferret_cleanup()
+	{
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'feed_links', 2);
+		remove_action('wp_head', 'feed_links_extra', 3);
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'start_post_rel_link', 10, 0);
+		remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+		remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+		remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+		remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_head', 'rel_canonical');
+		remove_action('wp_head', 'rel_alternate');
+		remove_action('wp_head', 'wp_oembed_add_discovery_links');
+		remove_action('wp_head', 'wp_oembed_add_host_js');
+		remove_action('wp_head', 'rest_output_link_wp_head');
+
+		remove_action('rest_api_init', 'wp_oembed_register_route');
+		remove_action('wp_print_styles', 'print_emoji_styles');
+
+		remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+		remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
+
+		add_filter('embed_oembed_discover', '__return_false');
+		add_filter('show_admin_bar', '__return_false');
+	}
+}
